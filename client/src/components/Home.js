77a48493e032cc3e52fffaa848ac248a230edc04
John Afolabi <johnafolabi.da@gmail.com>
2021-06-28 15:03:11 +0100

    Refactor Home to Functional components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,73 +1,46 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Grid, CssBaseline } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import { Grid, CssBaseline, makeStyles } from '@material-ui/core';
 import { SidebarContainer } from './Sidebar';
 import { ActiveChat } from './ActiveChat';
 import { fetchConversations } from '../store/utils/thunkCreators';
 
-const styles = {
+const useStyles = makeStyles(() => ({
   root: {
     height: '100vh',
   },
+}));
+
+const Home = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(fetchConversations());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setIsLoggedIn(true);
+  }, [user.id]);
+
+  if (!user.id) {
+    // If we were previously logged in, redirect to login instead of register
+    if (isLoggedIn) return <Redirect to="/login" />;
+    return <Redirect to="/register" />;
+  }
+
+  return (
+    <>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <SidebarContainer />
+        <ActiveChat />
+      </Grid>
+    </>
+  );
 };
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.user.id !== prevProps.user.id) {
-      this.setState({
-        isLoggedIn: true,
-      });
-    }
-  }
-
-  componentDidMount() {
-    this.props.fetchConversations();
-  }
-
-  render() {
-    const { classes } = this.props;
-    if (!this.props.user.id) {
-      // If we were previously logged in, redirect to login instead of register
-      if (this.state.isLoggedIn) return <Redirect to="/login" />;
-      return <Redirect to="/register" />;
-    }
-    return (
-      <>
-        <Grid container component="main" className={classes.root}>
-          <CssBaseline />
-          <SidebarContainer />
-          <ActiveChat />
-        </Grid>
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    conversations: state.conversations,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchConversations: () => {
-      dispatch(fetchConversations());
-    },
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Home));
+export default Home;
